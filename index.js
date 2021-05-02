bc5f5cccb7b2a9261c491ca077358973417d52ea
Harold Torres Marino <haroldtm55@gmail.com>
2021-05-03 00:01:12 +1000

    Test passed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names,event) {
+  let newArray = [];
+  for (let i = 0; i < names.length; i++) {
+    //console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    newArray[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+  }
+  return newArray;
+}
+
+//console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
+
+function countDown(n) {
+  let i=n;
+  while (i >= 0) {
+    i--
+    console.log(i+1);
+  }
+}
+
+//console.log(countdown(10));
